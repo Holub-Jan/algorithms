68e621fd54ec2c9f52bdfd5d4e9d0f082d79ae32
Jan Holub <jan.holub@1pf.cz>
2022-11-29 16:32:35 +0100

    Merge sort created.

---
+++ sort/main.py
@@ -0,0 +1,51 @@
+class Sort:
+    def __init__(self):
+        # To be added if needed
+        pass
+
+    def merge(self, lst):
+
+        if len(lst) <= 1:
+            return lst
+
+        left, right = self.divide_lst(lst)
+        result = []
+        left = self.merge(left)
+        right = self.merge(right)
+        pos_l = 0
+        pos_r = 0
+
+        while pos_l != len(left) and pos_r != len(right):
+            if left[pos_l] > right[pos_r]:
+                result.append(right[pos_r])
+                pos_r += 1
+            else:
+                result.append(left[pos_l])
+                pos_l += 1
+
+        if pos_l == len(left):
+            result += right[pos_r:]
+        else:
+            result += left[pos_l:]
+
+        return result
+
+    @staticmethod
+    def divide_lst(to_divide):
+        first_half = to_divide[:len(to_divide) // 2]
+        sec_half = to_divide[len(to_divide) // 2:]
+
+        return first_half, sec_half
+
+
+tests = [[3, 6, 2, 9, 12, 8, 0, 5, 1, 15, 20],
+         [12, 11, 13, 5, 6, 7],
+         [21, 1, 22, 23, 3, 4, 7, 6, 5, 25],
+         [],
+         [1]]
+
+sort = Sort()
+
+for data in tests:
+    print('Original data: ', data)
+    print('Sorted data: ', sort.merge(data), end='\n\n')
