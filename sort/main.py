d4f280598c229831407e10ab821c5cdd90776181
Jan Holub <jan.holub@1pf.cz>
2022-11-30 09:49:07 +0100

    Quick sort created.

--- sort/main.py
+++ sort/main.py
@@ -30,6 +30,26 @@ class Sort:
 
         return result
 
+    def quick(self, lst):
+
+        if len(lst) <= 1:
+            return lst
+
+        comp = lst[0]
+        # less = [value for value in lst[1:] if value < comp]
+        less = list()
+
+        # more = [value for value in lst[1:] if value >= comp]
+        more = list()
+
+        for value in lst[1:]:
+            if value < comp:
+                less.append(value)
+            else:
+                more.append(value)
+
+        return self.quick(less) + [comp] + self.quick(more)
+
     @staticmethod
     def divide_lst(to_divide):
         first_half = to_divide[:len(to_divide) // 2]
@@ -42,10 +62,11 @@ tests = [[3, 6, 2, 9, 12, 8, 0, 5, 1, 15, 20],
          [12, 11, 13, 5, 6, 7],
          [21, 1, 22, 23, 3, 4, 7, 6, 5, 25],
          [],
-         [1]]
+         [1],
+         [5, 5, 5, 5, 5, 5]]
 
 sort = Sort()
 
 for data in tests:
     print('Original data: ', data)
-    print('Sorted data: ', sort.merge(data), end='\n\n')
+    print('Sorted data: ', sort.quick(data), end='\n\n')
